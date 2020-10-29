712bc0341bb5dcf0e46c0ba5c186d0f18a531279
Stuart Fox <stuart@stuartfox.ca>
2020-10-29 00:07:27 -0700

    Add --delete to the sc cp command during build phase v3

--- static/build.sh
+++ static/build.sh
@@ -5,4 +5,4 @@ StaticAssetsBucket=$2
 
 cd static
 
-aws s3 sync --delete --recursive --exclude buildspec.yml --exclude build.sh ./ s3://"${StaticAssetsBucket}"/
+aws s3 sync --delete --exclude buildspec.yml --exclude build.sh ./ s3://"${StaticAssetsBucket}"/
